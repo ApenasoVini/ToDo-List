8d87186ba072841724378bcefef85e799f1045ac
vini <viniciusicmsc@gmail.com>
2024-07-08 16:30:43 -0300

    ToDo list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,24 @@
-export default function List(){
-  return (
-      <div>
-        
-      </div>
-  );
-};
+import { useState } from 'react';
 
+export default function List() {
+  const [finished, setFinished] = useState(false);
+
+  const endActivity = (): void => {
+    setFinished(!finished);
+  };
+
+  return (
+    <div className='List'>
+      <ul>
+        <li>
+          <p
+            onClick={endActivity}
+            style={{ textDecoration: finished ? 'line-through' : 'none' }}
+          >
+            Aaaa
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
